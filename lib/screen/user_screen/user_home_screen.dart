6b44fb3a175f13016196a1bdf4f6e69a7f108176
project-nazmul <project.nazmul@gmail.com>
2022-09-01 00:31:57 +0600

    user home page updated

--- lib/screen/user_screen/user_home_screen.dart
+++ lib/screen/user_screen/user_home_screen.dart
@@ -1,13 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerce_app/local_storage/local_storage.dart';
 import 'package:e_commerce_app/screen/login_screen.dart';
+import 'package:e_commerce_app/widget/custom_text.dart';
+import 'package:e_commerce_app/widget/text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 class UserHomeScreen extends StatelessWidget {
-  const UserHomeScreen({Key? key}) : super(key: key);
-
+  UserHomeScreen({Key? key}) : super(key: key);
+  final searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<LocalStorage>(context,listen: false);
@@ -26,29 +28,95 @@ class UserHomeScreen extends StatelessWidget {
           )
         ],
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection("products-data").snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-           if(snapshot.hasData){
-             return ListView.builder(
-               itemCount: snapshot.data!.docs.length,
-               itemBuilder: (context, index) {
-                 return Container(
-                   margin: EdgeInsets.all(8),
-                   height: 150,
-                   width: 150,
-                   decoration: BoxDecoration(
-                       image: DecorationImage(
-                           fit: BoxFit.fill,
-                           image: NetworkImage(snapshot.data!.docs[index]['img'].toString()))
-                   ),
-                 );
-               },
-             );
-           }
-           return Center(child: CircularProgressIndicator(),);
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10,right: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20,),
+            textField(icon: Icons.search,text: "Search products",controller:searchController ),
+            SizedBox(height: 40,),
+            customText(text: "All Products",clr: Colors.blue,fs: 22,fw: FontWeight.bold),
+            SizedBox(height: 10,),
+            Expanded(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance.collection("products-data").snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if(snapshot.hasData){
+                    return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          child: Row(
+                            children: [
+                              Expanded(flex:1,child: Container(
+                                margin: EdgeInsets.only(right: 15,),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: NetworkImage(snapshot.data!.docs[index]["img"])
+                                    )
 
-        },
+                                ),
+                              )),
+                              Expanded(flex:1,child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  customText(text: snapshot.data!.docs[index]['name'],clr: Colors.white,fw: FontWeight.bold,fs: 20),
+                                  SizedBox(height: 10,),
+                                  customText(text: "BDT "+snapshot.data!.docs[index]['price'],clr: Colors.black.withOpacity(0.8),fw: FontWeight.bold,fs: 15),
+                                ],
+                              )),
+                              Expanded(flex:1,child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 5),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: ElevatedButton(
+                                          onPressed: (){},
+                                          style: ButtonStyle(
+                                              backgroundColor: MaterialStatePropertyAll(Colors.blue.withOpacity(0.2))
+                                          ),
+                                          child: customText(text: "Details",clr: Colors.white,fw: FontWeight.bold,fs: 18)),
+                                    ),
+                                    SizedBox(height: 5,),
+                                    Expanded(
+                                      child: ElevatedButton(
+                                          style: ButtonStyle(
+                                              backgroundColor: MaterialStatePropertyAll(Colors.blue.withOpacity(0.2))
+                                          ),
+                                          onPressed: (){},
+                                          child: Icon(Icons.add_circle)
+                                      ),
+                                    ),
+
+                                  ],
+                                ),
+                              )),
+                            ],
+                          ),
+                          margin: EdgeInsets.only(bottom: 10),
+                          padding: EdgeInsets.only(left: 5,right: 5,bottom: 5,top: 5),
+                          height: 100,
+                          decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                        );
+                      },
+                    );
+                  }
+                  return Center(child: CircularProgressIndicator(),);
+
+                },
+              ),
+            )
+          ],
+        ),
       )
     );
   }
--- lib/widget/text_field.dart
+++ lib/widget/text_field.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-textField({IconData? icon, String? text, TextEditingController? controller, dynamic validator}){
+textField(
+    {IconData? icon,
+    String? text,
+    TextEditingController? controller,
+    dynamic validator}) {
   return TextFormField(
-    validator:validator,
+    validator: validator,
     controller: controller,
     decoration: InputDecoration(
-        prefixIcon: Icon(icon,color: Colors.blue,),
+      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue,width: 2)),
+        prefixIcon: Icon(icon,
+          color: Colors.blue,
+        ),
         label: Text(text!),
-        border: OutlineInputBorder()
-    ),
+        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.blue,width: 2))
+    )
   );
 }
